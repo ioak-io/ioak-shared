190f86fb33c23df581a04c646a71f058e8747810
Arun S <arun@ioak.io>
2025-08-27 17:59:29 +0530

    middleware in express test app to decode auth header

--- express-service/index.js
+++ express-service/index.js
@@ -1,10 +1,15 @@
-const express = require('express');
+const express = require("express");
+const middleware = require("./middleware");
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  console.log('Headers received:', req.headers);
-  res.send(req.headers);
+app.get("/", middleware, (req, res) => {
+  res.json({
+    message: "Claims extracted successfully",
+    claims: req.claims,
+    headers: req.headers
+  });
 });
 
 app.listen(port, () => {
